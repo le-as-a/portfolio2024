f02a4027c6237945d8d68367472950b66191107b
le-as-a <leasia0817@gmail.com>
2024-03-07 02:59:06 -0600

    transferred header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <>
-    
+      <img id="portfolio-title" src="portfolio-title.gif" alt="title" />
+      <p className='title' id="subtitle">Software Engineer & Certified Nerd</p>
+      <div className='navbar'>
+        <div className='nav-item'>About</div>
+        <div className='nav-item'>Skills</div>
+        <div className='nav-item'>Projects</div>
+        <div className='nav-item'>Games</div>
+      </div>
     </>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+function Header () {
+    return (
+        <>
+          <img id="portfolio-title" src="portfolio-title.gif" alt="title" />
+          <p className='title' id="subtitle">Software Engineer & Certified Nerd</p>
+          <div className='navbar'>
+            <div className='nav-item'>About</div>
+            <div className='nav-item'>Skills</div>
+            <div className='nav-item'>Projects</div>
+            <div className='nav-item'>Games</div>
+          </div>
+        </>
+      );
+}
+
+export default Header;
