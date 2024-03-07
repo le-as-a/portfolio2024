2b46352a6f0f37532d5a1d5a420da582905d5683
le-as-a <leasia0817@gmail.com>
2024-03-07 12:56:01 -0600

    about section done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './about.css';
 
 function About () {
     return (
         <>
             <div className='about'>
-                This is a test, wish me luck!
+                <img src='work-pfp.png' alt='pfp' /><br />
+                My name is Asia and I have a passion for software development, style and world building. 
+                Most of my early career was spent managing staff and customers at an Escape Room; I even had dreams of having my own Escape Room business one day! 
+                When COVID happened, I shifted goals and started a full-stack software engineer bootcamp program and graduated in 2022. <br /><br />
+                Since then, I've been expanding my skills and working on things I'm passionate on. Coding has been my platform for creativity the same way an artist 
+                has their pencils or a sculptor their clay. I love creating new things to help or entertain people!
             </div>
         </>
     )
