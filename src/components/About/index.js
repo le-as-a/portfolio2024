7d024fcc4b959c5cdb77dcbd7ab7713aa0d9acee
le-as-a <leasia0817@gmail.com>
2024-03-07 20:07:32 -0600

    added contact info

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,6 +11,17 @@ function About () {
                 When COVID happened, I shifted goals and started a full-stack software engineer bootcamp program and graduated in 2022. <br /><br />
                 Since then, I've been expanding my skills and working on things I'm passionate on. Coding has been my platform for creativity the same way an artist 
                 has their pencils or a sculptor their clay. I love creating new things to help or entertain people!
+                <div className='contact'>
+                    <a href="https://www.linkedin.com/in/asiaanhle/" rel="noreferrer" target="_blank">
+                        <img alt="linkedin" src="linkedin.png" width="50px" />
+                    </a>
+                    <a href="https://github.com/le-as-a" rel="noreferrer" target="_blank">
+                        <img alt="github" src="github.png" width="50px" />
+                    </a>
+                    <a href="Resume.docx" rel="noreferrer" target="_blank">
+                        <img alt="resume" src="cv.png" width="50px" />
+                    </a>
+                </div>
             </div>
         </>
     )
